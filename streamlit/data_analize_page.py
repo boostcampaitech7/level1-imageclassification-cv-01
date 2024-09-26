73f2e3dc1f9d320cf20cb076865a4f86404e0902
kimyoungseok3232 <rpfps3232@gmail.com>
2024-09-26 17:01:04 +0900

    Refactoring: streamlit 불필요한 코드 제거 및 주석 추가

--- streamlit/data_analize_page.py
+++ streamlit/data_analize_page.py
@@ -3,33 +3,34 @@ import pandas as pd
 
 st.set_page_config(page_title="데이터 분석 및 개별 이미지 확인용", layout="wide")
 
+# 최초에 데이터 불러와서 저장
 @st.cache_data
 def load_data():
     test_data = pd.read_csv('../data/test.csv')
     train_data = pd.read_csv('../data/train.csv')
     return test_data, train_data
 
+# 데이터 페이지 단위로 스플릿
 @st.cache_data(show_spinner=False)
 def split_frame(input_df, rows):
     df = [input_df.loc[i : i + rows - 1, :] for i in range(0, len(input_df), rows)]
     return df
 
+# 팝업창 띄우기
 @st.dialog("image")
 def show_image(type,path):
-    if type == "test":
-        st.header("test image: "+path)
-        st.image("../data/test/"+path)
-    elif type == "train":
-        st.header("train image: "+path)
-        st.image("../data/train/"+path)
+    pass
 
+# 페이지에 있는 이미지 출력
 def show_images(type, img_pathes, window):
     cols = window.columns(5)
     for idx,path in enumerate(img_pathes.values):
         cols[idx%5].image(f"../data/{type}/"+path)
         cols[idx%5].write(path)
 
+# 데이터 프레임 페이지 단위로 출력
 def show_dataframe(dataset,window,type):
+    # 가장 윗부분 데이터 정렬할 지 선택, 정렬 시 무엇으로 정렬할지, 오름차순, 내림차순 선택
     top_menu = window.columns(3)
     with top_menu[0]:
         sort = st.radio("Sort Data", options=["Yes", "No"], horizontal=1, index=1, key=[type,window,1])
@@ -43,11 +44,13 @@ def show_dataframe(dataset,window,type):
         dataset = dataset.sort_values(
             by=sort_field, ascending=sort_direction == "⬆️", ignore_index=True
         )
+    # 데이터 크기 출력
     total_data = dataset.shape
     with top_menu[0]:
         st.write("data_shape: "+str(total_data))
     con1,con2 = window.columns(2)
 
+    # 아래 부분 페이지당 데이터 수, 페이지 선택
     bottom_menu = window.columns((4, 1, 1))
     with bottom_menu[2]:
         batch_size = st.selectbox("Page Size", options=[10, 25, 50], key=[type,window,3])
@@ -63,20 +66,25 @@ def show_dataframe(dataset,window,type):
     pages = split_frame(dataset, batch_size)
     con1.dataframe(data=pages[current_page - 1], use_container_width=True)
     show_images(type, pages[current_page - 1]['image_path'], con2)
-    return pages[current_page - 1]
 
-option = st.sidebar.selectbox("데이터 선택",("CSV파일"))
+# 원본데이터 확인 가능 아웃풋도 확인하도록 할 수 있을 듯?
+option = st.sidebar.selectbox("데이터 선택",("원본 데이터"))
 
-if option == "CSV파일":
+if option == "원본 데이터":
+    # 데이터 로드
     testd, traind = load_data()
     if "path" not in st.session_state:
         st.session_state.path = [0,0]
-
-    targetdata = st.sidebar.checkbox("트레인 데이터 타겟 설정")
+    
+    # 테스트 데이터 출력
     st.header("테스트 데이터")
     page = show_dataframe(testd,st,'test')
 
+    # 트레인 데이터 출력
+    ## 타겟 별로 출력할지 설정
+    targetdata = st.sidebar.checkbox("트레인 데이터 타겟 설정")
     if not targetdata:
+        # 전체 출력
         st.header("트레인 데이터")
         show_dataframe(traind,st,'train')
         traintargetcount = traind["target"].value_counts().sort_index()
@@ -89,6 +97,7 @@ if option == "CSV파일":
         c1.dataframe(traincountcount, height=350)
         c2.bar_chart(traincountcount, height=350)
     else:
+        # 타겟 별 출력 시 어떤 타겟 출력할 것인지 선택
         target = st.sidebar.number_input("트레인 데이터 타겟 설정",min_value=traind['target'].min(),max_value=traind['target'].max(), step=1)
 
         traind = traind.loc[traind.target == target].reset_index()
