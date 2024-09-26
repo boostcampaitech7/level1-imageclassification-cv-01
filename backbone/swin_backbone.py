46847e4d58ff0b78404874526e8cc0a78a0e0b09
kkyungyoon <ruddbs803@gmail.com>
2024-09-27 00:34:01 +0900

    feat(model): modify Swin Transformer backbone

--- backbone/swin_backbone.py
+++ backbone/swin_backbone.py
@@ -5,9 +5,10 @@ class ModifiedSwinTransformer(nn.Module):
     def __init__(self, num_classes_original, num_classes_large, num_classes_small):
         super(ModifiedSwinTransformer, self).__init__()
         self.model = timm.create_model('swin_base_patch4_window7_224', pretrained=True)
-        self.model.head.fc = nn.Linear(self.model.num_features, num_classes_original)
-        self.large_classifier = nn.Linear(self.model.num_features, num_classes_large)
-        self.small_classifier = nn.Linear(self.model.num_features, num_classes_small)
+        self.model.head.fc = nn.Linear(self.model.head.fc.in_features, num_classes_original)
+        self.large_classifier = nn.Linear(self.model.layers[2].blocks[17].mlp.fc2.out_features, num_classes_large)
+        self.small_classifier = nn.Linear(self.model.layers[3].blocks[1].mlp.fc2.out_features, num_classes_small)
+
         self.intermediate_outputs = {}
         self._register_hooks()
 
@@ -17,14 +18,14 @@ class ModifiedSwinTransformer(nn.Module):
                 self.intermediate_outputs[name] = output
             return hook
 
-        self.model.layers[3].blocks[1].register_forward_hook(hook_fn('head_before_before'))
-        self.model.norm.register_forward_hook(hook_fn('head_before'))
+        self.model.layers[2].register_forward_hook(hook_fn('large'))
+        self.model.layers[3].register_forward_hook(hook_fn('small'))
 
     def forward(self, x):
         y_hat = self.model(x)
 
-        large_feature = self.intermediate_outputs['head_before_before']
-        small_feature = self.intermediate_outputs['head_before']
+        large_feature = self.intermediate_outputs['large']
+        small_feature = self.intermediate_outputs['small']
 
         large_output = self.large_classifier(large_feature.mean(dim=[1,2]))  # 대분류
         small_output = self.small_classifier(small_feature.mean(dim=[1,2]))  # 중분류
